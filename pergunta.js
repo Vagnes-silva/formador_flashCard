ffc443194095871aa43b46aa29393af51fdbc29d
Vagnes-silva <100873980+Vagnes-silva@users.noreply.github.com>
2024-11-14 15:02:47 -0300

    14/11

---
+++ pergunta.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programção',
+    'O que é Python',
+    'Python é uma linguagem de progração'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'A capital do Brasil e Brasilia'
+)
+
+criaCartao(
+    'Ciências',
+    'Qua a ciências que estuda os fenômenos da natureza',
+    'É a Física'
+)
